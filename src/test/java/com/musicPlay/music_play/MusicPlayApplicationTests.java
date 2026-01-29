9e3289df62da9d60bf4ff1a3226ce8536fed551d
Leidy <leidylorenatibocha9b@gmail.com>
2026-01-29 16:15:18 -0500

    primer commir en API REST Subscription API

---
+++ src/test/java/com/musicPlay/music_play/MusicPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicPlay.music_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
